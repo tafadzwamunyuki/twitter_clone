07b4f0ecc06a5ac1d0d21f004545fd456790a1f7
TAFADZWA MUNYUKI <tafadzwamunyuki@gmail.com>
2023-06-08 12:14:29 +0100

    News articles

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import { useState } from 'react'
+import News from './News'
 import { BsSearch } from 'react-icons/bs'
 
-export default function Widgets() {
+export default function Widgets({newsResults}) {
+    {/* Display 5 articles by default */}
+    const [articleNum, setArticleNum] = useState(5)
   return (
     <div className='xl:w-[600px] hidden lg:inline ml-8 space-y-5'>
       <div className='w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50'>
@@ -13,6 +16,17 @@ export default function Widgets() {
             />
         </div>
       </div>
+
+      <div className='text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]'>
+        <h4 className='font-bold text-xl px-4'>What`s happening</h4>
+        {newsResults.slice(0,articleNum).map((article) => (
+        <News key={article.title} article={article} />
+      ))}
+      {/* Activate show more button to show 5 more articles */}
+      <button onClick={() => setArticleNum(articleNum + 5)} className='text-blue-300 pl-4 pb-3 hover:text-blue-400'>Show more</button>
+      </div>
+
+      
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Feed from '@/components/Feed'
 import Widgets from '@/components/Widgets'
 
 
-export default function Home() {
+export default function Home({newsResults}) {
   return (
     <div>
       <Head>
@@ -14,17 +14,33 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='flex min-h-screen max-w-7xl mx-auto'>
+      <main className='flex min-h-screen mx-auto'>
       {/* Sidebar */}
         <Sidebar />
       {/* Feed */}
         <Feed />
       {/* Widgets */}
-      <Widgets />
+      <Widgets newsResults={newsResults.articles} />
       
       {/* Modal */}
       </main>
    
     </div>
   )
+}
+
+// https://saurav.tech/NewsAPI/top-headlines/category/business/in.json
+
+
+export async function getServerSideProps() {
+  const newsResults = await fetch ("https://saurav.tech/NewsAPI/top-headlines/category/business/in.json").then((res) =>res.json())
+
+  let randomUsersResults = [];
+  
+  return{
+    props:{
+      newsResults,
+      randomUsersResults,
+    },
+  }
 }
