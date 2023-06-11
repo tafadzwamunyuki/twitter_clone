987ddcfdf302e36c04fc64c72ac32f1d7bc846a1
TAFADZWA MUNYUKI <tafadzwamunyuki@gmail.com>
2023-06-11 09:52:08 +0100

    sign-in

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-clone-cb856.firebaseapp.com",
   projectId: "twitter-clone-cb856",
   storageBucket: "twitter-clone-cb856.appspot.com",
